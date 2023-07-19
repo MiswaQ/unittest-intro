d0af702f311b0f7f27f8aef603c4b9c8dbd3dbc6
MiswaQ <miswaq@gmail.com>
2023-07-19 01:51:42 +0000

    Cleaned the errors that made the file red

--- evens.py
+++ evens.py
@@ -11,7 +11,7 @@ def even_number_of_evens(numbers):
     if isinstance(numbers, list):
         evens = sum([1 for n in numbers if n % 2 == 0])
 
-        return True if evens and evens % 2 ==  0 else False
+        return True if evens and evens % 2 == 0 else False
     else:
         raise TypeError("A list was not passed into the function")
 
